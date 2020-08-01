c57c45e3f1969039c2bb0aad244755f3c2ab4531
Thiago Glayner <thiagoglayner_tg@hotmail.com>
2020-07-31 23:19:47 -0300

    finish lvl2

--- primeiro-projeto-ts-node/src/routes/users.routes.ts
+++ primeiro-projeto-ts-node/src/routes/users.routes.ts
@@ -10,19 +10,15 @@ const usersRoutes = Router();
 const upload = multer(uploadConfig);
 
 usersRoutes.post('/', async (request, response) => {
-  try {
-    const { name, email, password } = request.body;
-    const createUser = new CreateUserService();
-    const user = await createUser.execute({
-      name,
-      email,
-      password,
-    });
-    delete user.password;
-    return response.json(user);
-  } catch (error) {
-    return response.status(400).json({ error: error.message });
-  }
+  const { name, email, password } = request.body;
+  const createUser = new CreateUserService();
+  const user = await createUser.execute({
+    name,
+    email,
+    password,
+  });
+  delete user.password;
+  return response.json(user);
 });
 
 usersRoutes.patch(
@@ -30,19 +26,15 @@ usersRoutes.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    try {
-      const updateUserAvatar = new UpdateUserAvatarService();
-      const user = await updateUserAvatar.excecute({
-        user_id: request.user.id,
-        avatarFileName: request.file.filename,
-      });
+    const updateUserAvatar = new UpdateUserAvatarService();
+    const user = await updateUserAvatar.excecute({
+      user_id: request.user.id,
+      avatarFileName: request.file.filename,
+    });
 
-      delete user.password;
+    delete user.password;
 
-      return response.json(user);
-    } catch (error) {
-      return response.status(400).json({ error: error.message });
-    }
+    return response.json(user);
   },
 );
 export default usersRoutes;
