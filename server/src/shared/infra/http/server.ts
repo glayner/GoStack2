d374fcb870ab185b62ec2309ec368006a444a66f
Thiago Glayner <thiagoglayner_tg@hotmail.com>
2020-08-21 01:36:19 -0300

    finish

--- server/src/shared/infra/http/server.ts
+++ server/src/shared/infra/http/server.ts
@@ -2,16 +2,22 @@ import 'reflect-metadata';
 import 'dotenv/config';
 
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import { errors } from 'celebrate';
 import 'express-async-errors';
+
 import uploadconfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
+import rateLimiter from './middlewares/rateLimiter';
 import routes from './routes';
 
 import '@shared/infra/typeorm';
 import '@shared/container';
 
 const app = express();
+
+app.use(rateLimiter);
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadconfig.tmpFolder));
 app.use(routes);
